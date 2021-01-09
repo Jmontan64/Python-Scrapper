496165da390b1a10977d09726d9c050f23f01b9e
Jmontan64 <53578451+Jmontan64@users.noreply.github.com>
2021-01-09 15:55:51 -0500

    Update Web Scrapper.py

--- Web Scrapper.py
+++ Web Scrapper.py
@@ -7,15 +7,15 @@ driver.get('http://oxylabs.io/blog')
 results = []
 other_results = []
 content = driver.page_source
-soup = BeautifulSoup(content, features="html.parser")
+soup = BeautifulSoup(content)
 driver.quit()
 
-for a in soup.findAll(attrs='blog-cardcontent-wrapper'):
+for a in soup.findAll(attrs='blog-card__content-wrapper'):
     name = a.find('h2')
     if name not in results:
         results.append(name.text)
 
-for b in soup.findAll(attrs='blog-carddate-wrapper'):
+for b in soup.findAll(attrs='blog-card__date-wrapper'):
     date = b.find('p')
     if date not in results:
         other_results.append(date.text)
